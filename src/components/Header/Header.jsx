5babaa70883f4cd97c602b5dcc2aed169177e373
Dewmith Akalanka <akalankadewmith@gmail.com>
2024-09-19 13:27:58 +0530

    add stealth mode + ui features

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,51 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import Tooltip from "@mui/material/Tooltip";
+import Chip from "@mui/material/Chip";
+import { useChangeStealthMode, useGetUserById } from "../../hooks/useUserData";
+
 export default function Header() {
   const navigate = useNavigate();
   const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
+  const {
+    data: userData,
+    isLoading: isUserDataLoading,
+    isError: isUserDataError,
+    isSuccess: isUserDataSuccess,
+    error: userDataError,
+  } = useGetUserById(user?.id);
+
+  useEffect(() => {
+    if (isUserDataSuccess) {
+      console.log("triggered");
+      setisStealthModeEnabled(userData?.isStealthMode);
+    }
+  }, [isUserDataSuccess]);
+
+  const [isStealthModeEnabled, setisStealthModeEnabled] = useState(
+    userData?.isStealthMode
+  );
+
+  const {
+    mutate: changeStealthMutate,
+    isLoading: isChangeStealthLoading,
+    isError: isChangeStealthError,
+    isSuccess: isChangeStealthSuccess,
+    error: changeStealthError,
+  } = useChangeStealthMode();
+
+  useEffect(() => {
+    if (isChangeStealthSuccess) {
+      setisStealthModeEnabled(!isStealthModeEnabled);
+    }
+  }, [isChangeStealthSuccess, userData]);
+
   return (
     <div>
       {/* Header */}
@@ -47,27 +87,64 @@ export default function Header() {
                   fontWeight: "bold",
                 }}
                 onClick={() => {
-                  navigate("/dashboard");
+                  // setisStealthModeEnabled(!isStealthModeEnabled);
+                  changeStealthMutate({ userId: user?.id });
                 }}
               >
-                <span class="material-symbols-outlined">dashboard</span>
+                {isStealthModeEnabled ? (
+                  <>
+                    {/* <span>Stealth Mode Active</span> */}
+                    <Tooltip title="Turn OFF Stealth Mode">
+                      <Chip
+                        icon={<AdminPanelSettingsIcon color="inherit" />}
+                        label="Stealth Mode Active"
+                        color="primary"
+                        sx={{ color: "white" }}
+                      />
+                    </Tooltip>
+                  </>
+                ) : (
+                  <>
+                    <Tooltip title="Turn ON Stealth Mode">
+                      <AdminPanelSettingsOutlinedIcon />
+                    </Tooltip>
+                  </>
+                )}
               </button>
-              <button
-                className="btn btn-sm me-3"
-                style={{
-                  color: "white",
-                  fontFamily: "Noto Sans",
-                  fontSize: "14px",
-                  fontWeight: "bold",
-                }}
-                onClick={() => {
-                  dispatch({ type: "LOGOUT" });
-                  window.localStorage.removeItem("user");
-                  window.location.href = "/";
-                }}
-              >
-                <span class="material-symbols-outlined">logout</span>
-              </button>
+              <Tooltip title="Dashboard">
+                <button
+                  className="btn btn-sm me-3"
+                  style={{
+                    color: "white",
+                    fontFamily: "Noto Sans",
+                    fontSize: "14px",
+                    fontWeight: "bold",
+                  }}
+                  onClick={() => {
+                    navigate("/dashboard");
+                  }}
+                >
+                  <span class="material-symbols-outlined">dashboard</span>
+                </button>
+              </Tooltip>
+              <Tooltip title="Logout">
+                <button
+                  className="btn btn-sm me-3"
+                  style={{
+                    color: "white",
+                    fontFamily: "Noto Sans",
+                    fontSize: "14px",
+                    fontWeight: "bold",
+                  }}
+                  onClick={() => {
+                    dispatch({ type: "LOGOUT" });
+                    window.localStorage.removeItem("user");
+                    window.location.href = "/";
+                  }}
+                >
+                  <span class="material-symbols-outlined">logout</span>
+                </button>
+              </Tooltip>
             </div>
           </div>
         </header>
--- src/hooks/useUserData.js
+++ src/hooks/useUserData.js
@@ -71,8 +71,6 @@ export const useGetUserById = (userId) => {
   return useQuery({
     queryKey: ["getUserById", userId],
     queryFn: () => getUserById(userId),
-    staleTime: 1000 * 60 * 5, // 5 minutes stale time
-    // refetchInterval: 1000 * 60 * 15, // 15 minutes refetch interval
     enabled: !!userId,
   });
 };
@@ -100,5 +98,24 @@ export const useGetAllUsers = (currentUser) => {
     queryFn: () => getAllUsers(currentUser),
     staleTime: 1000 * 60 * 5, // 5 minutes stale time
     // refetchInterval: 1000 * 60 * 15, // 15 minutes refetch interval
+  });
+};
+
+const changeStealthMode = (data) => {
+  console.log(data);
+  const response = axios.post(baseUrl + `users/change-stealth-mode`, data);
+  return response.data;
+};
+
+export const useChangeStealthMode = () => {
+  return useMutation({
+    mutationFn: changeStealthMode,
+    onError: (err) => {
+      if (err.response) {
+        throw err.response.data.message;
+      } else {
+        throw err.message; // re-throw the error
+      }
+    },
   });
 };
